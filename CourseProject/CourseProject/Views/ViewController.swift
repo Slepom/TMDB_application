e0635ea4d633eddd4bf18346539fd07c6552f15f
Slepom <mynewlifebegin@gmail.com>
2022-09-28 18:42:28 +0300

    adaptation of the project to MVVM pattern

---
+++ CourseProject/CourseProject/Views/ViewController.swift
@@ -0,0 +1,21 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func button(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
