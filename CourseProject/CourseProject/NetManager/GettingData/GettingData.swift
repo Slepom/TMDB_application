2eb989eb74123d57aecc1985a49e32ee88e0c29c
Slepom <mynewlifebegin@gmail.com>
2022-10-13 18:24:30 +0300

    setup new model

--- CourseProject/CourseProject/NetManager/GettingData/GettingData.swift
+++ CourseProject/CourseProject/NetManager/GettingData/GettingData.swift
@@ -8,13 +8,14 @@ class GettingData{
     
     private init(){}
     
-    func getGenres(_ complitionHandler: @escaping (GenreModel) -> Void){
+    
+    func getGenres(_ complitionHandler: @escaping ([Genre]) -> Void){
         
         let request = AF.request("https://api.themoviedb.org/3/genre/movie/list?api_key=6cde63f94256f35e302a61f1dd4b7524&language=en-US", method: .get)
         request.responseDecodable(of: GenreModel.self) { response in
             
             do {
-                let genres = try response.result.get().self
+                let genres = try response.result.get().genres
                 
                 //print(genres)
                 complitionHandler(genres)                
@@ -42,5 +43,35 @@ class GettingData{
                     
                 }
         }
+    
+    
+    func createResulTest( _ complitionaHandler: @escaping ([Result]) -> Void){
+        
+        let request = AF.request("https://api.themoviedb.org/3/discover/movie?api_key=6cde63f94256f35e302a61f1dd4b7524&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate", method: .get)
+        request.responseDecodable(of: MovieModel.self) { response in
+
+            do {
+                let result = try response.result.get().results
+                complitionaHandler(result)
+               //print("?????????????????????????????????????????\(result)")
+            }
+            catch{
+                print("!!!!!!!!!\(error)")
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
    
 }
--- CourseProject/CourseProject/SecondScreen/Views/AuthenticationViewController.swift
+++ CourseProject/CourseProject/SecondScreen/Views/AuthenticationViewController.swift
@@ -4,22 +4,34 @@ import UIKit
 
 class AuthenticationViewController: UIViewController {
     
-    var dataSource: UICollectionViewDiffableDataSource<MovieModel, Result>!
-
+    //var dataSource: UICollectionViewDiffableDataSource<MovieModel, Result>!
+    //var dataSourceGenre: UICollectionViewDiffableDataSource<GenreModel, Genre>!
+    var dataSourceTest: UICollectionViewDiffableDataSource<Genre, Result>!
     var collectionView: UICollectionView!
     
-    var sections: [MovieModel] = []
+    //var sections: [MovieModel] = []
+    
+    var arrayGenre: [Genre] = []
 
+    var arrayResult: [Result] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
-        setupDataSourceData()
-        GettingData.shared.createListMovie { [weak self] dataMovieModel in
-            self?.sections = [dataMovieModel]
-            self?.reloadDataSections()
+        //setupDataSourceData()
+//        GettingData.shared.createListMovie { [weak self] dataMovieModel in
+//            self?.sections = [dataMovieModel]
+//            self?.reloadDataSections()
+//        }
+        setupDataSourceTest()
+        GettingData.shared.getGenres { [weak self] dataGenreModel in
+            self?.arrayGenre = dataGenreModel
 
         }
-    
+        GettingData.shared.createResulTest { [weak self] dataResult in
+            self?.arrayResult = dataResult
+            self?.reloadDataSectionsTest()
+        }
       
     }
     func setupCollectionView(){
@@ -40,49 +52,98 @@ class AuthenticationViewController: UIViewController {
         return cell
     }
     
-    func setupDataSourceData(){
-        dataSource = UICollectionViewDiffableDataSource<MovieModel, Result>(collectionView: collectionView)
-        { (collectionView, indexPath, result) -> UICollectionViewCell? in
-            switch self.sections[indexPath.section].page{
+//    func setupDataSourceData(){
+//        dataSource = UICollectionViewDiffableDataSource<MovieModel, Result>(collectionView: collectionView)
+//        { (collectionView, indexPath, result) -> UICollectionViewCell? in
+//            switch self.sections[indexPath.section].page{
+//            default:
+//                return self.configure(cellType: UserCell.self, with: result, for: indexPath)
+//            }
+//        }
+//    }
+    func setupDataSourceTest(){
+        dataSourceTest = UICollectionViewDiffableDataSource<Genre, Result>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, result) -> UICollectionViewCell? in
+            
+            switch self.arrayGenre[indexPath.section].name{
             default:
                 return self.configure(cellType: UserCell.self, with: result, for: indexPath)
             }
-        }
+            
+        })
+        
+        
     }
-    func reloadDataSections(){
-        var snapshots = NSDiffableDataSourceSnapshot<MovieModel, Result>()
-        snapshots.appendSections(sections)
-        for section in sections {
-            snapshots.appendItems(section.results, toSection: section)
-        }
-        dataSource.apply(snapshots)
+    func reloadDataSectionsTest(){
+        var snapshots = NSDiffableDataSourceSnapshot<Genre, Result>()
+        snapshots.appendSections(arrayGenre)
+      
+        snapshots.appendItems(arrayResult, toSection: arrayGenre.first)
+
+        dataSourceTest.apply(snapshots)
     }
 
+    
+    
+    
+    
+//    func reloadDataSections(){
+//        var snapshots = NSDiffableDataSourceSnapshot<MovieModel, Result>()
+//        snapshots.appendSections(sections)
+//        for section in sections {
+//            snapshots.appendItems(section.results, toSection: section)
+//        }
+//        dataSource.apply(snapshots)
+//    }
+
+//    func reloadDataGenre(){
+//        var snapshots = NSDiffableDataSourceSnapshot<GenreModel, Genre>()
+//        snapshots.appendSections(genre)
+//        for section in genre {
+//            snapshots.appendItems(section.genres, toSection: section)
+//        }
+//        dataSourceGenre.apply(snapshots)
+//    }
+    
+    
+    
 
     
     
      func createCompositionalLayout() -> UICollectionViewLayout{
         let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
-            let section = self.sections[sectionIndex]
-
-            switch section.page{
+            //let section = self.sections[sectionIndex]
+//
+            let section = self.arrayGenre[sectionIndex]
+            let result = self.arrayResult[sectionIndex]
+            switch section.id{
+            case 28 : return self.createFeaturedSection(using: result)
+            case 12 : return self.createFeaturedSection(using: result)
             default:
-               return self.createFeaturedSection(using: section)
+            return self.createFeaturedSection(using: result)
             }
-        }
-        let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.interSectionSpacing = 20
-        layout.configuration = config
-        return layout
+            
+            
+
+            
+//            switch section.results.first{
+//            default:
+//               return self.createFeaturedSection(using: section)
+//            }
+//        }
+       
     }
+         let config = UICollectionViewCompositionalLayoutConfiguration()
+         config.interSectionSpacing = 20
+         layout.configuration = config
+         return layout
     
-    
+     }
       
     
     
  
     
-     func createFeaturedSection(using section: MovieModel) -> NSCollectionLayoutSection{
+     func createFeaturedSection(using section: Result) -> NSCollectionLayoutSection{
         
 
          
