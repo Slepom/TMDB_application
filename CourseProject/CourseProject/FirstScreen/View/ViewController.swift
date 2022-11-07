c20b0d9f125c548884854db42aef186a0dd2dfc9
Slepom <mynewlifebegin@gmail.com>
2022-11-07 21:22:31 +0300

    create Search request and view

--- CourseProject/CourseProject/FirstScreen/View/ViewController.swift
+++ CourseProject/CourseProject/FirstScreen/View/ViewController.swift
@@ -3,20 +3,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var viewModelButton = ViewModelButton()
     
     
     @IBOutlet weak var loginTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
-        
+    
     @IBOutlet weak var checkLabel: UILabel!
     
     @IBAction func buttonAction(_ sender: Any) {
         checkPasswordLogin()
         NetworkManager.shared.posttoken()
         getSecondView()
+        
     }
     
     override func viewDidLoad() {
@@ -34,12 +35,12 @@ class ViewController: UIViewController {
         viewModelButton.statusText.bind({ (statusText) in
             DispatchQueue.main.async{
                 self.checkLabel.text = statusText
-
+                
             }
             
         })
     }
-
+    
     // MARK: - check login and password
     func checkPasswordLogin(){
         viewModelButton.checkSignUpButton(login: loginTextField.text ?? "", password: passwordTextField.text ?? " ")
@@ -50,6 +51,33 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
         vc.modalPresentationStyle = .fullScreen
     }
+    
+    // MARK: - add Navigarion & TabBar controllers
+    
+    func createNavigation(){
+        let tabBarVc = UITabBarController()
+        //        let vc1 = UINavigationController(rootViewController: SecondViewController())
+        //        let vc2 = UINavigationController(rootViewController: DetailsViewController())
+        //        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc1 = SecondViewController()
+        let vc2 = DetailsViewController()
+        let vc3 = SearchViewController()
+        vc1.title = " "
+        vc2.title = " "
+        vc3.title = " "
+        tabBarVc.setViewControllers([vc1,vc2,vc3 ], animated: false)
+        guard let items = tabBarVc.tabBar.items else{return}
+        
+        let images = ["house", "movie", "search"]
+        for namePictures in 0..<images.count{
+            items[namePictures].image = UIImage(systemName: images[namePictures])
+        }
+        
+        tabBarVc.modalPresentationStyle = .fullScreen
+        present(tabBarVc, animated: true)
+    }
+    
+    
     
 }
 
---
+++ CourseProject/CourseProject/SearchScreen/SearchViewCell.swift
@@ -0,0 +1,64 @@
+
+
+import UIKit
+import UIKit
+import SDWebImage
+
+class SearchViewCell: UICollectionViewCell{
+   
+    static var reuseId = "SearchCell"
+    let imageView = UIImageView()
+    let name = UILabel()
+    let subtitle = UILabel()
+    let overView = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        name.font = UIFont.preferredFont(forTextStyle: .headline)
+        name.textColor = .label
+        subtitle.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        subtitle.textColor = .secondaryLabel
+
+        imageView.clipsToBounds = true
+        imageView.frame = self.bounds
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 15
+       // imageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        //subtittle.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        overView.font = UIFont.preferredFont(forTextStyle: .body)
+        overView.textColor = .green
+        let innerStackView = UIStackView(arrangedSubviews: [name, subtitle, overView])
+        innerStackView.axis = .vertical
+        let otherStackView = UIStackView(arrangedSubviews: [imageView, innerStackView])
+        otherStackView.translatesAutoresizingMaskIntoConstraints = false
+        otherStackView.alignment = .center
+        otherStackView.spacing = 10
+        contentView.addSubview(otherStackView)
+
+        NSLayoutConstraint.activate([
+            otherStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            otherStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            otherStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            otherStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+       // otherStackView.setCustomSpacing(10, after: imageView)
+
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func configure(with result: Movie){
+        guard let posterPath = result.poster_path else{ return}
+        imageView.sd_setImage(with:(URL(string:"https://image.tmdb.org/t/p/w500" + posterPath)) ,completed: nil)
+        name.text = result.original_name ?? " "
+        subtitle.text = result.release_date ?? " "
+        overView.text = result.overview ?? " "
+    }
+    
+    
+}
+
